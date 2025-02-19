4cdd8927b9f745739efa42c686f99a739523a4f9
Shelley Gao <syugao@gmail.com>
2025-02-19 13:52:18 -0800

    fix: `scratch_event_store_data` return bug (#108)
    * remove fake head file
    * fix missing return

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,7 @@ from polyfactory.factories.pydantic_factory import ModelFactory
 from polyfactory.pytest_plugin import register_fixture
 from pydantic import BaseModel, TypeAdapter
 from pytest_mock import MockerFixture
+from structlog import get_logger
 
 from ref_builder.legacy.utils import build_legacy_otu
 from ref_builder.logs import configure_logger
@@ -28,6 +29,8 @@ from tests.fixtures.factories import (
 )
 
 configure_logger(True)
+
+logger = get_logger()
 
 
 @pytest.fixture(autouse=True)
