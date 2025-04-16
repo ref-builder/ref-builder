c5973fa13c900b17ebe464e652397af97781a899
Shelley Gao <syugao@gmail.com>
2025-04-16 11:48:44 -0700

    drop in replace

--- ref_builder/resources.py
+++ ref_builder/resources.py
@@ -2,7 +2,9 @@ import datetime
 
 from pydantic import UUID4, BaseModel, Field
 
-from ref_builder.otu.builders.deprecated import RepoSequence, RepoIsolate, RepoOTU
+from ref_builder.otu.builders.sequence import SequenceBuilder as RepoSequence
+from ref_builder.otu.builders.isolate import IsolateBuilder as RepoIsolate
+from ref_builder.otu.builders.otu import OTUBuilder as RepoOTU
 from ref_builder.utils import DataType
 
 
