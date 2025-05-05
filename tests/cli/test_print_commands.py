a1100602f7e3be620c7ef9fca58a77a084b3d759
Shelley Gao <syugao@gmail.com>
2025-05-05 13:26:04 -0700

    add TestPrintCommands.test_get_nonexistent_fail, .otu_get_deleted_fail

--- tests/cli/test_print_commands.py
+++ tests/cli/test_print_commands.py
@@ -1,7 +1,8 @@
+from uuid import uuid4
+
 from click.testing import CliRunner
 
 from ref_builder.cli.otu import otu as otu_command_group
-
 
 runner = CliRunner()
 
@@ -25,6 +26,39 @@ class TestPrintCommands:
             )
 
             assert result.exit_code == 0
+
+    def test_otu_get_nonexistent_fail(self, scratch_repo):
+        """Test that otu get command on a nonexistent OTU id fails with expected error text."""
+        result = runner.invoke(
+            otu_command_group,
+            ["--path", str(scratch_repo.path)] + ["get", str(uuid4())],
+        )
+
+        assert result.exit_code == 1
+
+        assert "OTU not found" in result.output
+
+    def test_otu_get_deleted_fail(self, scratch_repo):
+        """Test that otu get command on a deleted OTU id fails with expected error text."""
+        otu_id = scratch_repo.get_otu_id_by_taxid(438782)
+
+        with scratch_repo.lock(), scratch_repo.use_transaction():
+            scratch_repo.delete_otu(
+                otu_id,
+                rationale="Removing OTU for testing purposes",
+                replacement_otu_id=None,
+            )
+
+        assert scratch_repo.get_otu(otu_id) is None
+
+        result = runner.invoke(
+            otu_command_group,
+            ["--path", str(scratch_repo.path)] + ["get", str(otu_id)],
+        )
+
+        assert result.exit_code == 1
+
+        assert "OTU has been deleted" in result.output
 
     def test_otu_get_json_ok(self, scratch_repo):
         for otu_ in scratch_repo.iter_otus():
