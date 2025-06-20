31fd5892f04a65f9cf0634d87a5baf2403c3ddfa
Ian Boyes <igboyes@gmail.com>
2025-06-20 13:51:08 -0700

    test: print command output if cli tests fail

--- tests/cli/test_print_commands.py
+++ tests/cli/test_print_commands.py
@@ -1,97 +1,83 @@
-from uuid import uuid4
-
 from click.testing import CliRunner
 
 from ref_builder.cli.otu import otu as otu_command_group
+from ref_builder.repo import Repo
 
 runner = CliRunner()
 
 
-class TestPrintCommands:
+def test_otu_list(scratch_repo: Repo):
+    result = runner.invoke(
+        otu_command_group,
+        ["--path", str(scratch_repo.path)] + ["list"],
+    )
+
+    assert result.exit_code == 0
+
+
+class TestOTUGet:
     """Test otu console printing commands."""
 
-    def test_otu_list(self, scratch_repo):
-        result = runner.invoke(
-            otu_command_group,
-            ["--path", str(scratch_repo.path)] + ["list"],
-        )
-
-        assert result.exit_code == 0
-
-    def test_otu_get_ok(self, scratch_repo):
+    def test_plain(self, scratch_repo: Repo):
         for otu_ in scratch_repo.iter_otus():
             result = runner.invoke(
                 otu_command_group,
                 ["--path", str(scratch_repo.path)] + ["get", str(otu_.id)],
             )
 
+            if result.exit_code != 0:
+                print(result.output)
+
             assert result.exit_code == 0
 
-    def test_otu_get_nonexistent_fail(self, scratch_repo):
-        """Test that otu get command on a nonexistent OTU id fails with expected error text."""
-        result = runner.invoke(
-            otu_command_group,
-            ["--path", str(scratch_repo.path)] + ["get", str(uuid4())],
-        )
-
-        assert result.exit_code == 1
-
-        assert "OTU not found" in result.output
-
-    def test_otu_get_deleted_fail(self, scratch_repo):
-        """Test that otu get command on a deleted OTU id fails with expected error text."""
-        otu_id = scratch_repo.get_otu_id_by_taxid(438782)
-
-        with scratch_repo.lock(), scratch_repo.use_transaction():
-            scratch_repo.delete_otu(
-                otu_id,
-                rationale="Removing OTU for testing purposes",
-                replacement_otu_id=None,
-            )
-
-        result = runner.invoke(
-            otu_command_group,
-            ["--path", str(scratch_repo.path)] + ["get", str(otu_id)],
-        )
-
-        assert result.exit_code == 1
-
-        assert "OTU has been deleted" in result.output
-
-    def test_otu_get_json_ok(self, scratch_repo):
+    def test_json(self, scratch_repo: Repo):
+        """Test otu console printing commands with JSON output."""
         for otu_ in scratch_repo.iter_otus():
             result = runner.invoke(
                 otu_command_group,
                 ["--path", str(scratch_repo.path)] + ["get", str(otu_.id), "--json"],
             )
 
+            if result.exit_code != 0:
+                print(result.output)
+
             assert result.exit_code == 0
 
-    def test_otu_partial_ok(self, scratch_repo):
+    def test_partial_id(self, scratch_repo: Repo):
+        """Test that a partial ID can be used to retrieve OTUs."""
         for otu_ in scratch_repo.iter_otus():
             result = runner.invoke(
                 otu_command_group,
                 ["--path", str(scratch_repo.path)] + ["get", str(otu_.id)[:8]],
             )
 
+            if result.exit_code != 0:
+                print(result.output)
+
             assert result.exit_code == 0
 
-    def test_otu_partial_too_short(self, scratch_repo):
+    def test_otu_partial_too_short(self, scratch_repo: Repo):
+        """Test that a partial ID that is too short raises an error."""
         for otu_ in scratch_repo.iter_otus():
             result = runner.invoke(
                 otu_command_group,
                 ["--path", str(scratch_repo.path)] + ["get", str(otu_.id)[:5]],
             )
 
-            assert result.exit_code == 1
+            if result.exit_code != 1:
+                print(result.output)
 
+            assert result.exit_code == 1
             assert "OTU not found" in result.output
 
-    def test_otu_get_empty(self, scratch_repo):
+    def test_empty(self, scratch_repo):
+        """Test that an empty ID string raises an error."""
         result = runner.invoke(
             otu_command_group, ["--path", str(scratch_repo.path)] + ["get", ""]
         )
 
-        assert result.exit_code == 1
+        if result.exit_code != 1:
+            print(result.output)
 
+        assert result.exit_code == 1
         assert "OTU not found." in result.output
