3de54cc70bf6d2a7e8b98cfa10627f09a7810389
Ian Boyes <igboyes@gmail.com>
2025-10-03 14:23:36 -0700

    wip: add otu service

---
+++ ref_builder/services/__init__.py
@@ -0,0 +1,35 @@
+"""Repository services."""
+
+from ref_builder.ncbi.client import NCBIClient
+from ref_builder.repo import Repo
+
+
+class Service:
+    """Base class for repository services.
+
+    Provides access to the repository and NCBI client.
+    """
+
+    def __init__(self, repo: Repo, ncbi_client: NCBIClient) -> None:
+        """Initialize the service with a repository and NCBI client.
+
+        :param repo: the repository instance
+        :param ncbi_client: the NCBI client instance
+        """
+        self._repo = repo
+        self._ncbi = ncbi_client
+
+    @property
+    def repo(self) -> Repo:
+        """The repository instance."""
+        return self._repo
+
+    @property
+    def ncbi(self) -> NCBIClient:
+        """The NCBI client instance."""
+        return self._ncbi
+
+
+__all__ = [
+    "Service",
+]
